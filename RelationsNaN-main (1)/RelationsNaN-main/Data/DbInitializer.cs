536cc8d3a452b052319db6da941d0a0af12be137
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2025-08-29 12:04:53 -0400

    asdasdasdas
    dasdas

---
+++ RelationsNaN-main (1)/RelationsNaN-main/Data/DbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using RelationsNaN.Models;
+
+namespace RelationsNaN.Data
+{
+    public class DbInitializer
+    {
+        protected RelationsNaNContext _context;
+
+        public DbInitializer(RelationsNaNContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            if (_context.Game.Count() == 0)
+            {
+                Game game1 = new Game()
+                {
+                    Name = "Dark Souls III",
+                    ReleaseYear = 2016,
+                    Image = "https://image.api.playstation.com/cdn/UP0700/CUSA03388_00/v8JlD8KcQUtTqaLBmpFnj1ESRR5zMkLk.png"
+                };
+                await _context.Game.AddAsync(game1);
+
+                Game game2 = new Game()
+                {
+                    Name = "Super Smash Bros. Ultimate",
+                    ReleaseYear = 2018,
+                    Image = "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000012332/ac4d1fc9824876ce756406f0525d50c57ded4b2a666f6dfe40a6ac5c3563fad9"
+                };
+                await _context.Game.AddAsync(game2);
+
+                Game game3 = new Game()
+                {
+                    Name = "XCom 2",
+                    ReleaseYear = 2016,
+                    Image = "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/XCOM_2_cover_art.jpg/220px-XCOM_2_cover_art.jpg"
+                };
+                await _context.Game.AddAsync(game3);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ RelationsNaN-main (1)/RelationsNaN-main/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace RelationsNaN.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int ReleaseYear { get; set; }
+        public int? GenreId { get; set; }
+        [ValidateNever]
+        public Genre? Genre { get; set; }
+    }
+}
