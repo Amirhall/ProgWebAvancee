74d687a5edced4b90a0b06f10c7b916822614fdd
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2025-09-19 11:00:19 -0400

    tryytrtyr
    ytrytrytrtyr

--- PollingEtSignalR-main/PollingEtSignalR-main/PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
+++ PollingEtSignalR-main/PollingEtSignalR-main/PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
@@ -1,8 +1,70 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 
 namespace labo.signalr.api.Hubs
 {
     public class TaskHub : Hub
     {
+        private readonly ApplicationDbContext _context;
+
+        public TaskHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async override Task OnConnectedAsync( )
+        {
+            base.OnConnectedAsync();
+            IEnumerable<UselessTask> tasklistServer = await GetAllTasks();
+            await Clients.Client(Context.ConnectionId).SendAsync("tasklist", tasklistServer);
+        }
+
+
+        public async Task RecevoirUneNouvelleTache(string tache)
+        {
+
+            _context.UselessTasks.Add(new Models.UselessTask()
+            {
+                Text = tache,
+                Completed = false
+            });
+            await _context.SaveChangesAsync();
+
+            IEnumerable<UselessTask> tasklistServer = await GetAllTasks();
+            await Clients.All.SendAsync("tasklist", tasklistServer);
+        }
+
+        public async Task ChangerCompletion(int id)
+        {
+            UselessTask task = await _context.UselessTasks.FindAsync(id);
+            if (task == null)
+            {
+                return;
+            }
+
+
+            task.Completed = !task.Completed;
+            await _context.SaveChangesAsync();
+
+            IEnumerable<UselessTask> tasklistServer = await GetAllTasks();
+            await Clients.All.SendAsync("tasklist", tasklistServer);
+        }
+
+        // Méthode interne pour retourner uniquement la liste des tâches
+        private async Task<IEnumerable<UselessTask>> GetAllTasks()
+        {
+            return await _context.UselessTasks.ToListAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            base.OnDisconnectedAsync(exception);
+            // TODO: Ajouter votre logique
+        }
+
     }
 }
--- PollingEtSignalR-main/PollingEtSignalR-main/ngPollingEtSignalR/src/app/signalr/signalr.component.ts
+++ PollingEtSignalR-main/PollingEtSignalR-main/ngPollingEtSignalR/src/app/signalr/signalr.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import * as signalR from "@microsoft/signalr";
 import { UselessTask } from '../models/UselessTask';
-import { MatCheckbox } from '@angular/material/checkbox';
+import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatFormField, MatInput, MatInputModule, MatLabel } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-signalr',
   standalone: true,
-  imports: [MatCardContent,MatCard,MatFormField,FormsModule,MatLabel,CommonModule,MatInput,MatButtonModule],
+  imports: [MatCardContent,MatCard,MatFormField,FormsModule,MatLabel,CommonModule,MatInput,MatButtonModule, MatCheckboxModule],
   templateUrl: './signalr.component.html',
   styleUrls: ['./signalr.component.css']
 })
@@ -29,21 +29,34 @@ export class SignalrComponent implements OnInit {
 
   connecttohub() {
     // TODO On doit commencer par créer la connexion vers le Hub
+    this.hubConnection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7289/TaskHub").build();
    
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
-    
+    this.hubConnection!.on('tasklist', (data) => {
+      console.log(data);
+      this.tasks = data;
+    });
     // TODO On doit ensuite se connecter
+    this.hubConnection
+        .start()
+        .then(() => {
+            console.log('La connexion est active!');
+          })
+        .catch(err => console.log('Error while starting connection: ' + err));
+
     
   }
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
-   
+    this.hubConnection!.invoke('ChangerCompletion', id);
+    
   }
 
   addtask() {
-    // TODO On invoke la méthode pour ajouter une tâche sur le serveur
-    
+    this.hubConnection!.invoke('RecevoirUneNouvelleTache', this.taskname);
   }
+
+
 
 }
